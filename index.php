445da2d8f9e4821466b1467b11fc7f6f7260d83c
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2020-02-12 17:29:57 +0100

    Switching between drinks & food

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ function whatIsHappening() {
 }
 
 //your products with their price.
-$products = [
+$food = [
     ['name' => 'Club Ham', 'price' => 3.20],
     ['name' => 'Club Cheese', 'price' => 3],
     ['name' => 'Club Cheese & Ham', 'price' => 4],
@@ -25,40 +25,94 @@ $products = [
     ['name' => 'Club Salmon', 'price' => 5]
 ];
 
-$products = [
+$drink = [
     ['name' => 'Cola', 'price' => 2],
     ['name' => 'Fanta', 'price' => 2],
     ['name' => 'Sprite', 'price' => 2],
     ['name' => 'Ice-tea', 'price' => 3],
 ];
 
+//switching food & drinks
+$products = $drink;
+if (isset($_GET['food'])){
+    if($_GET['food'] ==1){
+        $products = $food;
+    }
+    else {
+        $products= $drink;
+    }
+}
+
+
+
+
+
 
 $totalValue = 0;
 
 $emailError = $strtnumError = $zipError =  $cityError =  $strtError = null;
 
+if (!empty($_POST['email'])) {
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-   // $emailError = "Email address " . $_POST['email'] . " is considered invalid.\n";
+        // $emailError = "Email address " . $_POST['email'] . " is considered invalid.\n";
         $emailError ="<div class= 'alert alert-danger'> Please enter a valid email address.</div>";
+    } else {
+        $_SESSION ['email'] = $_POST['email'];
     }
 
-    if (!is_numeric($_POST["streetnumber"])) {
-    // $strtnumError = "This  " . $_POST["streetnumber"] . " is an invalid street number!\n"; [This works!]
+}
+
+if (!empty($_POST['streetnumber'])){
+
+  if (is_numeric($_POST["streetnumber"])) {
+        // $strtnumError = "This  " . $_POST["streetnumber"] . " is an invalid street number!\n"; [This works!]
+        $strtnumError ="<div class='alert alert-success'> valid number!</div>";// Using bootstrap
+
+      $_SESSION ['streetnumber'] = $_POST['streetnumber'];
+    }
+    else {
         $strtnumError ="<div class='alert alert-danger'> This is not a number!</div>"; // Using bootstrap
     }
+}
 
-    if (!is_numeric($_POST["zipcode"])) {
-   // $zipError = $_POST["zipcode"] . " Invalid ZIP code!\n";
-        $zipError = "<div class='alert alert-danger'> Invalid ZIP code </div>";
+if (!empty($POST['zipcode'])){
+    if (is_numeric($_POST["zipcode"])) {
+        // $zipError = $_POST["zipcode"] . " Invalid ZIP code!\n";
+        $zipError ="<div class='alert alert-success'> valid ZIP code!</div>";
+        $_SESSION['zipcode'] = $_POST['zipcode'];
+    }
+    else {
+        $zipError ="<div class='alert alert-danger'> Invalid ZIP code</div>"; // Using bootstrap
     }
 
+}
+
+if (!empty($POST['city'])){
     if (!filter_var($_POST["city"])) {
         $cityError = "This is an invalid " . $_POST["city"] . " city!\n";
         $cityError = "<div class='alert alert-danger'> Input the city name</div>";
     }
+    else{$_SESSION['city'] = $_POST['city'];}
+}
 
-    if (empty($_POST["street"])) {
-        $strtError = "<div class='alert alert-danger'>Input the street name</div>". $_POST["street"];
+if(empty($POST['street'])) {
+    if (!filter_var($POST['street'])) {
+        $strtError = "<div class='alert alert-danger'>Input the street name</div>" . $_POST["street"];
+    } else {
+        $_SESSION['street'] = $_POST['street'];
     }
+}
+
+
+if (!$_SESSION['email'] or !$_SESSION['streetnumber'] or !$_SESSION['zipcode'] or !$_SESSION['city'] or !$_SESSION['street'])
+{
+    $_SESSION ['email'] = $_POST['email'];
+    $_SESSION['streetnumber'] = $_POST['streetnumber'];
+    $_SESSION['zipcode']= $_POST['zipcode'];
+    $_SESSION['city'] = $_POST['city'];
+    $_SESSION['street'] = $_POST['street'];
+}
+
+whatIsHappening();
 
 require './form-view.php';
